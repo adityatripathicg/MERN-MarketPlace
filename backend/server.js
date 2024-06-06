a66f5f42164c72d9413a66d4e59fa8523f862605
Aditya Tripathi <adityatripathi3906@gmail.com>
2024-06-06 18:32:57 +0530

    updated server.js acc. to new controllers and routes setup

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
-import Product from './models/product.model.js';
-import mongoose from 'mongoose';
+import productRoutes from "./routes/product.routes.js";
+
+
 dotenv.config();
 
 const app = express();
 
 app.use(express.json()); //allows us to accept JSON data in the req.body
+app.use("/api/products", productRoutes);
 
 app.listen(5000, ()=>{
     connectDB(); // Connect to MongoDB database. Replace 'your_database_name' with your actual database name.
