af0973f8ac4bb4c56bdbb5cef562ef3241533cdc
Aditya Tripathi <adityatripathi3906@gmail.com>
2024-06-08 19:04:53 +0530

    Updated PORT to .env and fixed some known issues

--- backend/controllers/product.controllers.js
+++ backend/controllers/product.controllers.js
@@ -1,4 +1,4 @@
-import Product from './models/product.model.js';
+import Product from '../models/product.model.js';
 import mongoose from 'mongoose';
 
 export const getProducts = async (req,res)=>{
@@ -44,11 +44,14 @@ export const updateProducts = async (req,res)=>{
 export const deleteProducts = async (req,res)=>{
     const {id} = req.params;
     console.log("ID : ", id);
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(400).json({success:false, message:"Invalid Product ID"});
+    }
     try {
         await Product.findByIdAndDelete(id);
         res.status(200).json({success:true, message:"Product deleted successfully"});
     } catch (error) {
-        res.status(404).json({success:false, message:"Product Not Found"});
+        res.status(500).json({success:false, message:"Product Not Found Server Error"});
     }
 
 }
--- backend/server.js
+++ backend/server.js
@@ -7,11 +7,12 @@ import productRoutes from "./routes/product.routes.js";
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000
 
 app.use(express.json()); //allows us to accept JSON data in the req.body
 app.use("/api/products", productRoutes);
 
-app.listen(5000, ()=>{
+app.listen(PORT, ()=>{
     connectDB(); // Connect to MongoDB database. Replace 'your_database_name' with your actual database name.
-    console.log('Server is running on port http://localhost:5000 \nHello CG Welcome Back!');
+    console.log("Server is running on port http://localhost:"+PORT+"\nHello CG Welcome Back");
 });
